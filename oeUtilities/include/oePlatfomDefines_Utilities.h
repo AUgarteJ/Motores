dd0dec7dbcfcc0f9f3fb70215f7846f2e82c969b
SAMUEL-LAPTOP\sam_s <samuel.prince.quezada@gmail.com>
2017-06-08 20:29:35 -0500

    Corrección al archivo por bug con dllexport y dllimport

--- oeUtilities/include/oePlatfomDefines_Utilities.h
+++ oeUtilities/include/oePlatfomDefines_Utilities.h
@@ -192,18 +192,15 @@
 
 //! Windows
 #if OE_ACTIVE_PLATFORM == OE_PLATFORM_WINDOWS
-#if defined(OE_UTILITIES_STATIC_LIB)
-#define OE_UTILITIES_EXPORT
-#else
-#if defined(OE_UTILITIES_EXPORTS)
-#define OE_UTILITIES_EXPORT __declspec(dllexport)
-#else
-#if defined (__MINGW32__)
-#define OE_UTILITIES_EXPORT	
-#else
-#define	OE_UTILITIES_EXPORT __declspec(dllexport)
-#endif
-#endif
+# if defined(OE_UTILITIES_STATIC_LIB)
+#   define OE_UTILITIES_EXPORT 
+# else
+#   if defined(OE_UTILITIES_EXPORTS)
+#     define OE_UTILITIES_EXPORT __declspec(dllexport)
+#   else
+#     define OE_UTILITIES_EXPORT __declspec(dllimport)
+#   endif
+# endif
 #endif
 
 #if defined(_DEBUG) || defined(DEBUG)
@@ -214,7 +211,6 @@
 
 #if OE_ACTIVE_COMPILER == OE_COMPILER_INTEL
 #define OE_THREAD_LOCAL __declspec(thread)
-#endif
 #endif
 
 // !Linux and Mac
